407839569b9f66798f9ea07672c75860d742e6b0
Pobitrodeb <pobitrodn@gmail.com>
2023-06-01 23:19:14 +0600

    users, categories, and product migration added

--- database/migrations/2023_06_01_144059_create_products_table.php
+++ database/migrations/2023_06_01_144059_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description', 1000);
+            $table->string('quantity')->unsigned();
+            $table->string('status')->default(Product::UNAVIABLE_PRODUCT);
+            $table->text('image');
+            $table->integer('seller_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
+
         });
     }
 
